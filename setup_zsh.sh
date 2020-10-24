bed5413866e39072e74a6074d2e2f94159519ced
r888800009 <r888800009@gmail.com>
2020-10-25 01:03:12 +0800

    Update setup_zsh.sh

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -7,10 +7,13 @@ echo install zsh plugin
 mkdir -p ~/.antigen
 curl -L git.io/antigen > ~/.antigen/antigen.zsh
 chmod -R 755 ~/.antigen
-#if [[ ! -z $PM_pacman ]]; then
+if [[ ! -z $PM_pacman ]]; then
 #    sudo pacman -S zsh-syntax-highlighting
-#elif [[ ! -z $PM_apt ]];then
+  echo
+elif [[ ! -z $PM_apt ]];then
 #    sudo apt install zsh-syntax-highlighting
-#fi
+  echo
+  sudo apt install autojump
+fi
 
 
